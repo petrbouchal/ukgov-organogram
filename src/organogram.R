0a15def07b7bae58561de980a3bfa7b964b3d88b
Petr Bouchal <pbouchal@gmail.com>
2014-07-01 10:48:27 +0100

    First R code to analyse organograms

---
+++ src/organogram.R
@@ -0,0 +1,21 @@
+# library(pbtools)
+# library(plyr)
+library(dplyr)
+library(data.table)
+library(stringr)
+
+junior <- read.csv('./data-input/Cabinet_Office_31_March_2014_junior_data.csv')
+
+summary(junior)
+summary(senior)
+
+junior <- as.data.table(junior)
+
+senior$name[senior$fte==min(senior$fte)]
+
+senior <- read.csv('./data-input/Cabinet_Office_31_March_2014_senior_data.csv')
+senior <- as.data.table(senior)
+names(senior) <- tolower(str_replace_all(names(senior),'\\.',''))
+senior <- senior %>% 
+  group_by(postuniquereference) %>%
+  mutate(seniorreports = n())
